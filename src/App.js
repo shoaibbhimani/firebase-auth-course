82aecac54929cfca150ecd1e4cf78176defc2f37
shoaibbhimani <shoaibbhimani1392@gmail.com>
2021-04-25 04:54:07 +0530

    Chakra & React Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom"
+
+const Dashboard = () => <div>Dashboard</div>
+
+const Login = () => <div>Login</div>
+
+const Signup = () => <div>Signup</div>
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Switch>
+           <Route  exact path="/" component={Dashboard} />
+           <Route path="/login" component={Login} />
+           <Route path="/signup" component={Signup} />
+        </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from "@chakra-ui/react"
+import { BrowserRouter } from "react-router-dom"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <BrowserRouter>
+         <App /> 
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
