4d758f807c2f31e174e6acec8e5b33ce038b4c14
shoaibbhimani <shoaibbhimani1392@gmail.com>
2021-04-25 17:25:22 +0530

    Code Formatting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route } from "react-router-dom";
 
-const Dashboard = () => <div>Dashboard</div>
+const Dashboard = () => <div>Dashboard</div>;
 
-const Login = () => <div>Login</div>
+const Login = () => <div>Login</div>;
 
-const Signup = () => <div>Signup</div>
+const Signup = () => <div>Signup</div>;
 
 function App() {
   return (
     <div className="App">
-        <Switch>
-           <Route  exact path="/" component={Dashboard} />
-           <Route path="/login" component={Login} />
-           <Route path="/signup" component={Signup} />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ChakraProvider } from "@chakra-ui/react"
-import { BrowserRouter } from "react-router-dom"
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-         <App /> 
+        <App />
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
